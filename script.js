8a612dc95ccd145cc40e2b2fb5af69010c40c68c
connork96 <connorkelly96@yahoo.com>
2022-08-03 19:05:14 -0400

    return answer to generate password

--- script.js
+++ script.js
@@ -12,10 +12,10 @@ function writePassword() {
 
 }
 
-var upper = 'A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z'
-var lower = 'a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z'
-var number = '0,1,2,3,4,5,6,7,8,9'
-var symbol = '!,@,#,$,%,^,&,*,(,)'
+var upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+var lower = 'abcdefghijklmnopqrstuvwxyz'
+var number = '0123456789'
+var symbol = '!@#$%^&*()'
 
 function generatePassword() {
 
@@ -24,57 +24,58 @@ var typesArray = [];
 
 
   var passwordlength = prompt("How Many Characters Would You Like Your Password To Have?");
-  if ( length <8 || length >128 )
-   { alert.prompt("please Enter a Number Between 8 and 128");
+  if ( passwordlength <8 || passwordlength >128 ) {
+     alert("please Enter a Number Between 8 and 128");
    
   }
   else {
 
   };
 
-  var upper = window.confirm("Would You Like Your Password to Contain Uppercase Letters?")
-  if (upper) {
+  var upperConfirm = window.confirm("Would You Like Your Password to Contain Uppercase Letters?")
+  if (upperConfirm) {
     typesArray.push(upper)
-    typesCount++
   } else {
 };
 
-  var lower = window.confirm("Would You Like Your Password to Contain Lowercase Letters?")
-  if (lower) {
+  var lowerConfirm = window.confirm("Would You Like Your Password to Contain Lowercase Letters?")
+  if (lowerConfirm) {
   typesArray.push(lower)
-  typesCount++
   } else {
 
   };
 
-  var number = window.confirm("Would You Like Your Password to Contain Numbers?")
-  if (number) {
+  var numberConfirm = window.confirm("Would You Like Your Password to Contain Numbers?")
+  if (numberConfirm) {
     typesArray.push(number)
-    typesCount++ 
   } else {
     
   };
-
-  var symbol = window.confirm("Would You Like Your Password to Contain Special Characters?")
-  if (symbol) {
+                                               
+  var symbolConfirm = window.confirm("Would You Like Your Password to Contain Special Characters?")
+  if (symbolConfirm) {
     typesArray.push(symbol)
-    typesCount++
   } else {
     
   };
 
+console.log(typesArray)
 
+ 
+var answer = ''
 
-  //console.log('typescount', typesCount)
-
-  if (typesCount === 0) {
-    return " ";
-  }
 
   for(let i = 0; i <= passwordlength; i++) {
-var randomNumber = Math.floor(Math.random() * typesArray.length);
-  password = typesArray.substring(randomNumber, randomNumber + 1);
+var randomNumber1 = Math.floor(Math.random() * typesArray.length);
+ var randomItem = typesArray[randomNumber1]
+ var randomNumber2 = Math.floor(Math.random() * randomItem.length);
+ //console.log(randomItem[randomNumber2])
+answer = answer + randomItem[randomNumber2]
+
+
   } 
+  
+  return answer;
 };
 
 // Add event listener to generate button
