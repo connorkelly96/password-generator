b83829f64d90e348fdde3aa448923eefe2675646
connork96 <connorkelly96@yahoo.com>
2022-08-02 22:46:41 -0400

    just discovered charcode to replace arrays and created getrandomlower function

--- script.js
+++ script.js
@@ -2,13 +2,32 @@
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
+
+
+function getRandomLower() {
+
+
+
+}
+
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  passwordText.value = password; 
 
 }
+
+function generatePassword() {
+  var numberOfChars = prompt("How Many Characters Would You Like Your Password To Have?");
+  if ( number <8 || number >128 ) {
+    prompt("Please Select a Number Between 8 and 128")
+  };
+
+
+  return 
+};
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
