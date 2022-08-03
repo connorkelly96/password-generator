f9bca9fdf0890c15b9a84f0be4f6792a3c11eb2b
connork96 <connorkelly96@yahoo.com>
2022-08-02 23:02:42 -0400

    created getrandomsymbol function using an array

--- script.js
+++ script.js
@@ -17,6 +17,11 @@ function getRandomNumber() {
   return String.fromCharCode(Math.floor(Math.random() * 10) + 48)
 }
 
+function getRandomSymbol() {
+  const symbols = '!@#$%^&*()<>+-[]{}='
+  return symbols[Math.floor(Math.random() * symbols.length)];
+}
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
