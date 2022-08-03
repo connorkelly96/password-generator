27a78f8f15b6fc60e935b0dbb93b45b05055db48
connork96 <connorkelly96@yahoo.com>
2022-08-02 22:54:53 -0400

    created getrandomnumber function

--- script.js
+++ script.js
@@ -6,11 +6,16 @@ var generateBtn = document.querySelector("#generate");
 
 function getRandomLower() {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-
-
-
 }
 
+function getRandomUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+
+function getRandomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48)
+}
 
 function writePassword() {
   var password = generatePassword();
