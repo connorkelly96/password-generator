7f080d00407d8fde213788645fb0120711238f25
connork96 <connorkelly96@yahoo.com>
2022-08-03 17:12:32 -0400

    created function to generate password

--- script.js
+++ script.js
@@ -3,30 +3,6 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 
-var randomFunction = {
-lower: getRandomLower,
-upper: getRandomUpper,
-number: getRandomNumber,
-symbol: getRandomSymbol
-};
-
-function getRandomLower() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-}
-
-function getRandomUpper() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-}
-
-
-function getRandomNumber() {
-  return String.fromCharCode(Math.floor(Math.random() * 10) + 48)
-}
-
-function getRandomSymbol() {
-  const symbols = '!@#$%^&*()<>+-[]{}='
-  return symbols[Math.floor(Math.random() * symbols.length)];
-}
 
 function writePassword() {
   var password = generatePassword();
@@ -36,15 +12,18 @@ function writePassword() {
 
 }
 
-
+var upper = 'A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z'
+var lower = 'a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z'
+var number = '0,1,2,3,4,5,6,7,8,9'
+var symbol = '!,@,#,$,%,^,&,*,(,)'
 
 function generatePassword() {
 
-let generatedPassword = " ";
-var typesArray = [];
-var typesCount = 0;
 
-  var length = prompt("How Many Characters Would You Like Your Password To Have?");
+var typesArray = [];
+
+
+  var passwordlength = prompt("How Many Characters Would You Like Your Password To Have?");
   if ( length <8 || length >128 )
    { alert.prompt("please Enter a Number Between 8 and 128");
    
@@ -81,7 +60,7 @@ var typesCount = 0;
     typesArray.push(symbol)
     typesCount++
   } else {
-
+    
   };
 
 
@@ -92,15 +71,10 @@ var typesCount = 0;
     return " ";
   }
 
-  for(let i = 0; i < length; i += typesCount) {
-    typesArray.forEach(type => {
-      var functionName = Object.keys(type)[0]; 
-      console.log('randomFunction:', randomFunction)
-
-      generatedPassword += randomFunction[functionName]();
-      console.log(generatedPassword)
-    } );
-  }
+  for(let i = 0; i <= passwordlength; i++) {
+var randomNumber = Math.floor(Math.random() * typesArray.length);
+  password = typesArray.substring(randomNumber, randomNumber + 1);
+  } 
 };
 
 // Add event listener to generate button
